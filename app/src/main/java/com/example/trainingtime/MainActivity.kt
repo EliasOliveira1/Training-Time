55d89e71b8842ab9b2e7269d53488ebf4bb95a02
EliasOliveira1 <tiofilep@gmail.com>
2021-12-11 13:12:13 -0800

    Create button to create training

--- app/src/main/java/com/example/trainingtime/MainActivity.kt
+++ app/src/main/java/com/example/trainingtime/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.trainingtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.content.Intent
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var create_training = findViewById(R.id.createTraining) as Button
+
+        create_training.setOnClickListener {
+            val intent = Intent(this, CreateTraining::class.java)
+            // start your next activity
+            startActivity(intent)
     }
-}
+}}
